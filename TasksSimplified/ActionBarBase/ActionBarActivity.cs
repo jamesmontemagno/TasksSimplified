a22ba9e54dd2cba2e8f88e70ac8671956d6c037f
James Montemagno <james.montemagno@gmail.com>
2012-07-09 20:07:22 -0700

    Fixed menu for base activity.

--- TasksSimplified/ActionBarBase/ActionBarActivity.cs
+++ TasksSimplified/ActionBarBase/ActionBarActivity.cs
@@ -33,6 +33,9 @@ namespace TasksSimplified.ActionBarBase
             if(ActionBar == null)
                 return base.OnPrepareOptionsMenu(menu);
 
+            menu.Clear();
+            MenuInflater.Inflate(IsDarkTheme ? DarkMenuId : MenuId, menu);
+
             for (int i = 0; i < menu.Size(); i++)
             {
                 var menuItem = menu.GetItem(i);
